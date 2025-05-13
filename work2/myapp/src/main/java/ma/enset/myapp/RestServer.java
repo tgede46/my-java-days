242a17c2831c78d9f39f63b2c681c7f844e72e8e
Gedeonkp <gedeonkpara@gmail.com>
2025-05-13 08:02:36 +0000

    corretion projet

--- work2/myapp/src/main/java/ma/enset/myapp/RestServer.java
+++ work2/myapp/src/main/java/ma/enset/myapp/RestServer.java
@@ -7,7 +7,7 @@ public class RestServer {
     public static void main (String[] args){
         Gson gson= new Gson();
         get("/hello",(req,resp)->{
-            Map<String,Object> data= new HashMap();
+            Map<String,Object> data=new HashMap<>();
             data.put("name", "Alfred");
             data.put("sala", "6500");
             resp.type("applcation/json");
