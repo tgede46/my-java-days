ffb4c87878299bc924d20b80e5de9a302ddd20dc
tgede46 <gedeonkpara@gmail.com>
2025-05-25 19:25:31 +0000

    "add : new feature to reponse "

---
+++ work4/src/main/java/ma/enset/service/StandardResponse.java
@@ -0,0 +1,20 @@
+package ma.enset.service;
+
+public class StandardResponse {
+
+    private StatusResponse status;
+    private String message;
+    private JsonElement data;
+
+    public StandardResponse(StatusResponse status) {
+        // ...
+    }
+    public StandardResponse(StatusResponse status, String message) {
+        // ...
+    }
+    public StandardResponse(StatusResponse status, JsonElement data) {
+        // ...
+    }
+
+    // getters and setters
+}
